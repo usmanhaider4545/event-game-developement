8d577c6e0e8381d16a7e2ce1ea61d35c1b261d0f
Toseef Shahid <toseef.shahid@intagleo.com>
2023-09-27 16:14:44 +0500

    Added background image as gif

--- event/src/App.js
+++ event/src/App.js
@@ -2,14 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/home';
 import './style.css';
-import background from "./images/ezgif-4-ebe4b003ca.gif";
-
+ 
 function App() {
 
   return (
-    <div className="App" style={{ backgroundImage: `url(${background})`, backgroundRepeat: "no-repeat", backgroundSize: "cover"  }}>
-        <Home />
-      </div>
+    <div className="App" style={{ backgroundColor: "#0f172a" }}>
+      <Home />
+    </div>
   );
 }
 
